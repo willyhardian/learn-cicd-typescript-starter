7fa4c62b1cec459ae2f31f35d895369353c842c4
Willy Hardian <hardian.will@gmail.com>
2025-10-12 09:00:24 +0700

    add linting and try ci workflow lint error

---
+++ eslint.config.ts
@@ -0,0 +1,30 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  {
+    // --> Add this ignores block at the top <--
+    ignores: [
+      "dist/",
+      "node_modules/",
+      "coverage/", // Add this line
+    ],
+  },
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node, // <-- ADD THIS LINE
+        ...globals.browser, // Optional: if you also use browser globals
+      },
+    },
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: { globals: globals.browser },
+  },
+  tseslint.configs.recommended,
+]);
